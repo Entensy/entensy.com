a60d757bed2d381cc536138f7945e67f46e20dc7
Rawand201 <ra.one77@outlook.com>
2023-12-10 21:08:33 +0300

    Enable fonts and default colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typogrpahy from '@tailwindcss/typography';
 import queries from '@tailwindcss/container-queries';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
@@ -11,6 +12,41 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        ckb: ['rabar', 'Arial', 'montserrat'],
+        en: ['montserrat', 'rabar', 'Arial'],
+      },
+      colors: {
+        primary: '#FC002A',
+        accent: {
+          light: '#999999',
+          dark: '#404040',
+        },
+        secondery: {
+          light: '#D9D9D9',
+          dark: '#0d0d0d',
+        },
+        danger: {
+          light: colors.rose[400],
+          normal: colors.rose[500],
+          dark: colors.rose[600],
+        },
+        warning: {
+          light: colors.yellow[400],
+          normal: colors.yellow[500],
+          dark: colors.yellow[600],
+        },
+        success: {
+          light: colors.green[400],
+          normal: colors.green[500],
+          dark: colors.green[600],
+        },
+        info: {
+          light: colors.blue[400],
+          normal: colors.blue[500],
+          dark: colors.blue[600],
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
