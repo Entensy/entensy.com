9c1fe0e59774681b06acc90d8ac6c66895fae7ad
AlanD20 <aland20@pm.me>
2024-03-03 20:17:03 +0100

    Add nav menu

--- app.vue
+++ app.vue
@@ -1,24 +1,31 @@
 <script setup>
-  import {ref, provide} from 'vue'
+import { ref, provide } from 'vue'
 
-  const navigation = ref({
-    state: false,
-    toggle(){
-      console.log('yep');
-      this.state = !this.state;
-    },
-  })
-  provide('navigation', navigation)
+const navigation = ref({
+  state: false,
+  toggle() {
+    this.state = !this.state;
+  },
+  close() {
+    this.state = false
+  }
+})
+
+provide('navigation', navigation)
+
+onMounted(() => {
+  window.addEventListener('resize', _ => navigation.value.close());
+})
 
 </script>
-<template>
-    <AppHeader />
-    <Hero />
-    <Service />
-    <Portfolio />
-    <About />
-    <Faq />
-    <ContactUs />
-    <AppFooter />
-</template>
 
+<template>
+  <AppHeader />
+  <Hero />
+  <Service />
+  <Portfolio />
+  <About />
+  <Faq />
+  <ContactUs />
+  <AppFooter />
+</template>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,16 +1,13 @@
-<template>
-  <div v-motion :initial="{
-    y: -50,
-    opacity: 0.15,
+<script setup>
+import { inject } from "vue";
 
-  }" :enter="{
-    y: 0,
-    opacity: 1,
-    transition: {
-      delay: 25,
-      duration: 500,
-    },
-  }" class="sticky top-0 z-50 flex items-center justify-between w-full px-16 pt-12 ">
+const navigation = inject('navigation')
+</script>
+
+<template>
+  <div v-motion :initial="{ y: -50, opacity: 0.15, }"
+    :enter="{ y: 0, opacity: 1, transition: { delay: 25, duration: 500, } }"
+    class="sticky top-0 z-50 flex items-center justify-between w-full px-16 pt-12 ">
     <NuxtLink to="/">
       <img src="/images/logo.png" class="w-16 h-10" alt="logo">
     </NuxtLink>
@@ -27,7 +24,81 @@
         contact us
       </button>
     </div>
-      <button class="p-4 text-white capitalize border rounded-full border-white/15 bg-blur">-</button>
+    <div class="flex lg:hidden">
+      <button @click="navigation.toggle()"
+        class="p-4 text-white capitalize border rounded-full border-white/15 bg-blur">
+        <Transition name="rotation" mode="out-in">
+          <svg v-if="navigation.state" viewBox="0 0 20 20" fill="currentColor" width="32" height="32"
+            color="var(--token-9ba5df47-566d-4cce-965a-b909bddf5adc, rgb(204, 204, 204))"
+            class="transition-all duration-150" style="width: 100%; height: 100%;">
+            <path fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"></path>
+          </svg>
+          <svg v-else-if="!navigation.state" viewBox="0 0 20 20" fill="currentColor" width="32" height="32"
+            color="var(--token-9ba5df47-566d-4cce-965a-b909bddf5adc, rgb(204, 204, 204))"
+            style="width: 100%; height: 100%;" class="transition-all duration-150">
+            <path fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"></path>
+          </svg>
+        </Transition>
+      </button>
+
+      <Transition name="bounce" mode="out-in">
+        <div v-if="navigation.state"
+          class="block lg:hidden absolute top-32 left-12 right-12 text-white border rounded-2xl border-white/15 drop-shadow backdrop-blur-lg">
+          <div class="flex flex-col items-center justify-center gap-8 px-12 py-4 capitalize text-md">
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/about">About</NuxtLink>
+            <NuxtLink to="/contact">Contact</NuxtLink>
+            <NuxtLink to="/portfolio">portfolio </NuxtLink>
+          </div>
+        </div>
+      </Transition>
+
     </div>
   </div>
 </template>
+
+<style>
+.bounce-enter-active {
+  animation: bounce-in 400ms;
+}
+
+.bounce-leave-active {
+  animation: bounce-in 400ms reverse;
+}
+
+.rotation-enter-active {
+  animation: rotate 300ms;
+}
+
+.rotation-leave-active {
+  animation: rotate 300ms reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+
+  50% {
+    transform: scale(1.10);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+
+  100% {
+    transform: rotate(180deg);
+  }
+}
+</style>
